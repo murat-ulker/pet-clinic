62edc5abb77cadf070ee6ed94fafb5a662b0be79
themulker <themulker@gmail.com>
2022-09-26 20:39:28 +0300

    demo

--- pet-clinic-kotlin/src/main/kotlin/com/github/rskupnik/petclinic/repositories/PetRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.github.rskupnik.petclinic.repositories
-
-import com.github.rskupnik.petclinic.model.Pet
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface PetRepository : CrudRepository<Pet, Long>
